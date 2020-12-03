fec4bd6fb228d14f0906d577587cbbaede79829c
noncelab <noncelab@noncelab.com>
2020-12-03 15:39:50 +0900

    commit!

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,17 +1,27 @@
 import * as React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import UnityAds from 'react-native-unity-ads';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
   React.useEffect(() => {
-    UnityAds.multiply(3, 7).then(setResult);
+    UnityAds.initialized('3923800', 'video', true)
   }, []);
+
+  const showAd = () => {
+
+    UnityAds.adShow().then(result=>{
+      console.log(result);
+    }).catch(error=>{
+      console.log(error);
+    });
+
+  }
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Button onPress={showAd} title={"test"} />
     </View>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import { NativeModules } from 'react-native';
 
 type UnityAdsType = {
   multiply(a: number, b: number): Promise<number>;
+  initialized(gameId : string, placementId : string, test : boolean) : void;
+  isLoad() : Promise<boolean>;
+  adShow() : Promise<any>;
+
 };
 
 const { UnityAds } = NativeModules;
